bcf72e0e01309165f19bd09785f3b099c7f94b85
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-10-10 02:44:40 -0500

    fix cors

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -9,6 +9,5 @@ module.exports = (app) => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(cors({origin: config.clientUrl}));
-  app.options('*', cors({origin: config.clientUrl}));
   app.use(override());
 };
